56d24c7b936c400f6809674713674bcace090aeb
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-03-06 07:51:49 -0600

    Initial Commit

---
+++ FocusExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusExample
+//
+//  Created by Justin Hold on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusExample/FocusExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusExampleApp.swift
+//  FocusExample
+//
+//  Created by Justin Hold on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FocusExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
